bug fix. odom.theta overflow

catkin_ws/src/the_car/src/cmd_vel_listener.py
@@ -53,7 +53,7 @@ def move(linear, angular):
 
 
 def check_odom(odom_broadcaster, odom_pub):
-    # rospy.loginfo("odm.updated="+str(real_car.odm.updated))
+    rospy.loginfo("odm.updated="+str(real_car.odm.updated))
     if real_car.odm.updated:
         x, y, theta, time_stamp = real_car.odm.get_odom()
         quat = tf.transformations.quaternion_from_euler(0, 0, theta)