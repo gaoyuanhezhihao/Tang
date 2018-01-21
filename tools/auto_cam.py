d7c4d77d7656bef0ae754871c2e2273da6766bce
gaoyuanhezhihao <gaoyuanhezhihao@gmail.com>
2018-01-21 11:48:10 +0800

    auto cam tools. EasyLogger link for ROS Node

--- tools/auto_cam.py
+++ tools/auto_cam.py
@@ -1,14 +1,22 @@
 from __future__ import print_function
 import cv2
 from time import time, sleep
+import os
+import glob
 
 freq = 5
 intv = 1.0/freq
 
-cap = cv2.VideoCapture(1)
-cap.set(cv2.cv.CV_CAP_PROP_FPS, freq)
+cap = cv2.VideoCapture(0)
+# cap.set(cv2.cv.CV_CAP_PROP_FPS, freq)
+cap.set(3, 1280)
+cap.set(4, 720)
 idx = 0
 tm = time()
+dest = os.path.expanduser("~/car/data/vd_cap/")
+map(os.unlink, glob.glob(dest+"*"))
+tmpl = dest+"%d.jpg"
+print(tmpl)
 while True:
     gap = time() - tm
     print(gap)
@@ -18,5 +26,5 @@ while True:
     ret, im = cap.read()
     cv2.imshow("video", im)
     cv2.waitKey(1)
-    cv2.imwrite("./vd_cap/%d.jpg" % idx, im)
+    cv2.imwrite(tmpl% idx, im)
     idx += 1
